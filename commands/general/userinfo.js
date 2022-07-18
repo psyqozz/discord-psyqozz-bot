abac484150ef969bbf4ca2662d25afd924153670
psyqozz <dylan.juzaine@gmail.com>
2022-07-18 20:35:15 +0200

    chore(user) get userinfo

--- commands/general/userinfo.js
+++ commands/general/userinfo.js
@@ -7,16 +7,22 @@ exports.help = {
 
 exports.run = async (client, message, args) => {
     const username = message.mentions.users.first() ? message.mentions.users.first() : message.author;
+    let member = message.guild.members.cache.get(username.id);
+    let nickname = member ? member.displayName : username.username;
 
     const userInfoEmbed = new MessageEmbed()
 	.setColor(config.embed.color)
-    .setAuthor({ name: 'Psyqozz_', iconURL: config.embed.logoServeur})
-	.setDescription(`Ìnformation sur l'utilisateur ${username.username}`)
-	.setThumbnail(config.embed.logoServeur)
+    .setAuthor({ name: `Information sur ${nickname} `, iconURL: username.displayAvatarURL()})
+	.setDescription(`Information sur l'utilisateur ${username.tag}`)
+	.setThumbnail(username.displayAvatarURL())
 	.addFields(
-		{ name: '**ID du membre**', value: `${username.id}`, inline: true },
-		{ name: '**Pseudo sur le serveur**', value: `${username.nickname ? username.nickname : username.username}`, inline: true },
-		{ name: '**Date de création du compte**', value: `${username.createdAt.toLocaleDateString('fr-FR')}` },
+		{ name: '**ID**', value: `${username.id}`},
+		{ name: '**Pseudo**', value: `${nickname}`},
+        { name: '**Modérateur**', value: `${member.kickable ? '🟢' : '🔴'}`, inline: true },
+        { name: '**Bot**', value: `${member.user.bot ? '🟢' : '🔴'}`, inline: true},
+        { name: '**Rôles**', value: `${member.roles.cache.map(role => role).join(',')}`},
+        { name: '**A rejoint le serveur le**', value: `<t:${parseInt(member.joinedTimestamp / 1000)}:f> (<t:${parseInt(member.joinedTimestamp / 1000)}:R>)`, inline: true },
+        { name: '**A crée son compte le**', value: `<t:${parseInt(member.user.createdTimestamp / 1000)}:f> (<t:${parseInt(member.user.createdTimestamp / 1000)}:R>)`, inline: true },
 	)
 	.setFooter({ text: config.embed.thanks, iconURL: config.embed.picture });
 
