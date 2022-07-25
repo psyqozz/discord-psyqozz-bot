d50a60963f13076627664eb8a9edba360a78c655
psyqozz <dylan.juzaine@gmail.com>
2022-07-26 01:27:36 +0200

    chore(twitter): lint code

--- socialNetwork/twitter.js
+++ socialNetwork/twitter.js
@@ -22,46 +22,46 @@ async function sendMessage (tweet, client) {
   }
 }
   
-  async function listenForever(streamFactory, dataConsumer) {
-    try {
-      for await (const { data } of streamFactory()) {
-        dataConsumer(data);
-      }
-        // The stream has been closed by Twitter. It is usually safe to reconnect.
-        console.log('Stream disconnected healthily. Reconnecting.');
-        listenForever(streamFactory, dataConsumer);
-    } catch (error) {
-        // An error occurred so we reconnect to the stream. Note that we should
-        // probably have retry logic here to prevent reconnection after a number of
-        // closely timed failures (may indicate a problem that is not downstream).
-        console.log('Stream disconnected with error. Retrying.', error);
-        listenForever(streamFactory, dataConsumer);
+async function listenForever(streamFactory, dataConsumer) {
+  try {
+    for await (const { data } of streamFactory()) {
+      dataConsumer(data);
     }
+      // The stream has been closed by Twitter. It is usually safe to reconnect.
+      console.log('Stream disconnected healthily. Reconnecting.');
+      listenForever(streamFactory, dataConsumer);
+  } catch (error) {
+      // An error occurred so we reconnect to the stream. Note that we should
+      // probably have retry logic here to prevent reconnection after a number of
+      // closely timed failures (may indicate a problem that is not downstream).
+      console.log('Stream disconnected with error. Retrying.', error);
+      listenForever(streamFactory, dataConsumer);
+  }
+}
+
+module.exports = async(client) => {
+  const endpointParameters = {
+    'tweet.fields': [ 'author_id', 'conversation_id' ],
+    'expansions': [ 'author_id', 'referenced_tweets.id' ],
+    'media.fields': [ 'url' ]
   }
 
-  module.exports = async(client) => {
-    const endpointParameters = {
-      'tweet.fields': [ 'author_id', 'conversation_id' ],
-      'expansions': [ 'author_id', 'referenced_tweets.id' ],
-      'media.fields': [ 'url' ]
+  try {
+    console.log('Setting up Twitter....')
+    const body = {
+      "add": [
+        {"value": `from:${config.twitter.tweet_username}`, "tag": "from Me!!"}
+      ]
     }
+    const r = await t.post("tweets/search/stream/rules", body);
+    console.log(r);
 
-    try {
-      console.log('Setting up Twitter....')
-      const body = {
-        "add": [
-          {"value": `from:${config.twitter.tweet_username}`, "tag": "from Me!!"}
-        ]
-      }
-      const r = await t.post("tweets/search/stream/rules", body);
-      console.log(r);
-  
-    } catch (err) {
-      console.log(err)
-    }
- 
-    listenForever(
-    () => t.stream('tweets/search/stream', endpointParameters),
-    (data) => sendMessage(data, client)
-    );
+  } catch (err) {
+    console.log(err)
+  }
+
+  listenForever(
+  () => t.stream('tweets/search/stream', endpointParameters),
+  (data) => sendMessage(data, client)
+  );
 }
